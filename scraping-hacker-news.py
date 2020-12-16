c73bfe7d905349e91125a23df1f023fd92c8cb28
iancarpenter <ianhcarpenter@gmail.com>
2020-12-16 21:19:16 +0000

    nothing near complete

--- scraping-hacker-news.py
+++ scraping-hacker-news.py
@@ -8,14 +8,15 @@ from bs4 import BeautifulSoup
 PAGE = "https://news.ycombinator.com/"
 OK = 200
 
-def get_hacker_news_information(soup):
-    '''Return a dictionary with the information from Hackernews'''
-    news = soup.find_all('table', 'itemlist')
+# def get_hacker_news_information(soup):
+#     '''Return a dictionary with the information from Hackernews'''
+    
+#     # news = soup.find_all('table', class_='itemlist')
 
-    print(news[0])
+#     #print(news[0])
 
-    # for n in news:
-    #     print(n)
+#     for i in soup.find_all('table', class_='itemlist').find('tr'):
+#         print(i.find('a', class_='storylink').text)        
         
 
 
@@ -24,6 +25,7 @@ def get_hacker_news_information(soup):
 
 
 if __name__ == "__main__":
+    
     result = requests.get(PAGE)
 
     assert result.status_code == 200, f"Got status code {result.status_code} \ which isn't a success"
@@ -32,6 +34,14 @@ if __name__ == "__main__":
 
     soup = BeautifulSoup(source, 'html.parser')
 
+    results = soup.find_all('td', class_='title')
+
+    for result in results:
+        #print(result)
+        title = result.find('a', class_='storylink')
+        print(title)
+        
+
     #print(soup)
 
-    get_hacker_news_information(soup)
+    #get_hacker_news_information(soup)
