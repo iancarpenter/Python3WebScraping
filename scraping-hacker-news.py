155c408ff60d24b534917877a3c1b736a7f0b14d
iancarpenter <ianhcarpenter@gmail.com>
2020-12-31 16:57:07 +0000

    added docstrings

--- scraping-hacker-news.py
+++ scraping-hacker-news.py
@@ -8,42 +8,61 @@ from bs4 import BeautifulSoup
 
 PAGE = "https://news.ycombinator.com/"
 OK = 200
-CSV_HEADER = ["rank", "title", "site_str", "score", "hn_user" , "age", "comment_count"]
+CSV_HEADER = ["rank", "title", "site_str",
+              "score", "hn_user", "age", "comment_count"]
 CSV_FILE_NAME = 'hn.csv'
 
-def first_row_of_hacker_news(result):
-    
+
+def first_row_of_hacker_news(story):
+    ''' The first row of Hacker news contains the Rank, Title and the site address
+        These are extracted into variables and returned to the caller. 
+
+        Args: story: string containing a HTML fragment within which the required data
+        is located 
+
+        Returns: Strings: three variables rank_rtn, title_rtn, site_str_rtn
+    '''
+
     rank_rtn = ''
     title_rtn = ''
     site_str_rtn = ''
-     
-    rank = result.find('span', class_='rank')
+
+    rank = story.find('span', class_='rank')
     if rank is not None:
         rank_rtn = rank.text
 
-    title = result.find('a', class_='storylink')        
-    if title is not None:            
+    title = story.find('a', class_='storylink')
+    if title is not None:
         title_rtn = title.text
 
-    site_str = result.find('span', class_='sitestr')
+    site_str = story.find('span', class_='sitestr')
     if site_str is not None:
         site_str_rtn = site_str.text
-    
+
     return rank_rtn, title_rtn, site_str_rtn
 
 
 def second_row_of_hacker_news(hn_second_line, counter):
-        
+    ''' The second row of Hacker news contains the score, hn_user, age, comment_count
+
+        These are extracted into variables and returned to the caller. 
+
+        Args: hn_second_line: list containing a HTML fragments within which the required data
+                              is located 
+              counter: int which row from the list is required
+
+        Returns: Strings: four variables; score_rtn, hn_user_rtn, age_rtn, comment_count_rtn
+    '''
     score_rtn = ''
     hn_user_rtn = ''
     age_rtn = ''
     comment_count_rtn = ''
 
-    score = hn_second_line[counter].find('span', class_='score')        
+    score = hn_second_line[counter].find('span', class_='score')
     if score is not None:
         score_rtn = score.text
 
-    hn_user = hn_second_line[counter].find('a', class_='hnuser')        
+    hn_user = hn_second_line[counter].find('a', class_='hnuser')
     if hn_user is not None:
         hn_user_rtn = hn_user.text
 
@@ -51,23 +70,31 @@ def second_row_of_hacker_news(hn_second_line, counter):
     age = age_enclosing_tag.find('a')
     if age is not None:
         age_rtn = age.text
-    
-    # the comment count is in a block of html that requires some extra steps to extract. 
+
+    # the comment count is in a block of html that requires some extra steps to extract.
     # Two solutions I found, one using a lambda and one using find_next_sibling
-    #comment_count = hn_second_line[counter].find(lambda tag:tag.name=="a" and "comments" in tag.text)        
-    comment_count = hn_second_line[counter].find('span', class_='age').find_next_sibling('a').find_next_sibling('a')
-    
-    if comment_count is not None:              
+    #comment_count = hn_second_line[counter].find(lambda tag:tag.name=="a" and "comments" in tag.text)
+    comment_count = hn_second_line[counter].find(
+        'span', class_='age').find_next_sibling('a').find_next_sibling('a')
+
+    if comment_count is not None:
         comment_count_rtn = comment_count.text
 
     return score_rtn, hn_user_rtn, age_rtn, comment_count_rtn
 
+
 def get_html():
-    
+    ''' Using the requests library for handling HTTP and beautiful soup to parse the HTML document
+
+       Args: None
+
+       Return: soup: Parsed HTML document of the Hacker News website        
+    '''
+
     result = requests.get(PAGE)
 
-    assert result.status_code == 200, f"Got status code {result.status_code} \\ which isn't a success"
-    
+    assert result.status_code == OK, f"Got status code {result.status_code} \\ which isn't a success"
+
     source = result.text
 
     soup = BeautifulSoup(source, 'html.parser')
@@ -76,45 +103,63 @@ def get_html():
 
 
 def extract_hn_to_list(hn_first_line, hn_second_line):
-    
+    ''' Extracts information from the HTML fragments for the HacknerNews 
+        first and and second lines and returns a list containing the information
+        combined from both lines.
+        
+        Args: HTML fragments 
+
+        Return: list: Containing the information for both lines combined
+
+    '''
     counter = 0
 
-    data = []
+    first_and_second_rows_combined = []
 
-    for result in hn_first_line:
-        
-        rank, title, site_str = first_row_of_hacker_news(result)
-        
-        score, hn_user, age, comment_count = second_row_of_hacker_news(hn_second_line, counter)                
+    for story in hn_first_line:
 
-        data.append([rank, title, site_str, score, hn_user, age, comment_count])
+        rank, title, site_str = first_row_of_hacker_news(story)
 
-        counter += 1  
-    
-    return data
+        score, hn_user, age, comment_count = second_row_of_hacker_news(
+            hn_second_line, counter)
+
+        first_and_second_rows_combined.append([rank, title, site_str, score,
+                     hn_user, age, comment_count])
+
+        counter += 1
+
+    return first_and_second_rows_combined
 
 
 def create_csv_file(hn_list):
     with open(CSV_FILE_NAME, 'w', newline='') as myfile:
-        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)        
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
         wr.writerow(CSV_HEADER)
         for i in hn_list:
             wr.writerow(i)
 
 
 def scrape_hn_front_page():
-        
+    '''Entry point into the module. 
+    
+    Gets the Hacker news HTML then extracts the first and second line details for each story
+    These are combined and added to a list which is then written out into a CSV file. 
+    
+    Args: None
+
+    Returns: None
+    '''
+
     soup = get_html()
-    
+
     hn_first_line = soup.find_all('tr', class_='athing')
-    
+
     hn_second_line = soup.find_all('td', 'subtext')
-        
-    hn_list = extract_hn_to_list(hn_first_line, hn_second_line)                    
+
+    hn_list = extract_hn_to_list(hn_first_line, hn_second_line)
 
     create_csv_file(hn_list)
 
-                
+
 if __name__ == "__main__":
-    scrape_hn_front_page()    
-    
+    scrape_hn_front_page()
