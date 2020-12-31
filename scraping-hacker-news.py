382ba6375dbc14b4376de8272050481847e1695c
iancarpenter <ianhcarpenter@gmail.com>
2020-12-31 11:51:28 +0000

    writing out csv

--- scraping-hacker-news.py
+++ scraping-hacker-news.py
@@ -2,6 +2,7 @@
 Produces a CSV file in the same directory where this module is run from
 containing the title, site, score, user id, comment count
 """
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,34 +11,47 @@ OK = 200
 
 
 def first_row_of_hacker_news(result):
+    
+    rank_rtn = ''
+    title_rtn = ''
+    site_str_rtn = ''
+
     # first row of hacker news  
     rank = result.find('span', class_='rank')
     if rank is not None:
-        print('the rank is ' + rank.text)
+        rank_rtn = rank.text
 
     title = result.find('a', class_='storylink')        
     if title is not None:            
-        print('the title is ' + title.text)
+        title_rtn = title.text
 
     site_str = result.find('span', class_='sitestr')
     if site_str is not None:
-        print('the site_str is ' + site_str.text)
+        site_str_rtn = site_str.text
+    
+    return rank_rtn, title_rtn, site_str_rtn
 
 
 def second_row_of_hacker_news(hn_second_line, counter):
     # second row of hacker news
+    
+    score_rtn = ''
+    hn_user_rtn = ''
+    age_rtn = ''
+    comment_count_rtn = ''
+
     score = hn_second_line[counter].find('span', class_='score')        
     if score is not None:
-        print('the score is ' + score.text)
+        score_rtn = score.text
 
     hn_user = hn_second_line[counter].find('a', class_='hnuser')        
     if hn_user is not None:
-        print('the hn user is ' + hn_user.text)
+        hn_user_rtn = hn_user.text
 
     age_enclosing_tag = hn_second_line[counter].find('span', class_='age')
     age = age_enclosing_tag.find('a')
     if age is not None:
-        print('the age is ' + age.text) 
+        age_rtn = age.text
     
     # the comment count is in a block of html that requires some extra steps to extract. 
     # Two solutions I found, one using a lambda and one using find_next_sibling
@@ -45,8 +59,9 @@ def second_row_of_hacker_news(hn_second_line, counter):
     comment_count = hn_second_line[counter].find('span', class_='age').find_next_sibling('a').find_next_sibling('a')
     
     if comment_count is not None:              
-        print('the comment count is ' + comment_count.text)
+        comment_count_rtn = comment_count.text
 
+    return score_rtn, hn_user_rtn, age_rtn, comment_count_rtn
 
 def get_html():
     
@@ -61,27 +76,46 @@ def get_html():
     return soup
 
 
-def scrape_hn_front_page():
+def extract_hn_to_list(hn_first_line, hn_second_line):
     
+    counter = 0
+
+    data = []
+
+    for result in hn_first_line:
+
+        # top row of hacker news  
+        rank, title, site_str = first_row_of_hacker_news(result)
+
+        # second row of hacker news
+        score, hn_user, age, comment_count = second_row_of_hacker_news(hn_second_line, counter)                
+
+        data.append([rank, title, site_str, score, hn_user, age, comment_count])
+
+        counter += 1  
+    
+    return data
+
+
+def scrape_hn_front_page():
+        
     soup = get_html()
     
     hn_first_line = soup.find_all('tr', class_='athing')
     
     hn_second_line = soup.find_all('td', 'subtext')
         
-    counter = 0
+    hn_list = extract_hn_to_list(hn_first_line, hn_second_line)                    
 
-    for result in hn_first_line:
-                                
-        # top row of hacker news  
-        first_row_of_hacker_news(result)
-
-        # second row of hacker news
-        second_row_of_hacker_news(hn_second_line, counter)                
-        
-        counter += 1                    
+    #for i in hn_list:
+    #    print(i)
+    with open('hn.csv', 'w', newline='') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        for i in hn_list:
+            wr.writerow(i)
 
 
+                
 if __name__ == "__main__":
     scrape_hn_front_page()    
     
